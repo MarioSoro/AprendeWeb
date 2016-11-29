ca9e61fe07bc3427b10598c25af200abd0f3bb4b
MarioSoro <orosgomez@gmail.com>
2016-11-29 12:34:48 +0100

    Add files via upload

---
+++ aprendeweb/index.js
@@ -0,0 +1,28 @@
+function layoutHandler()
+	{	var styleLink = document.getElementById("estilo");
+		if(window.innerWidth < 620)
+		{	styleLink.setAttribute("href", "min.css");
+		}
+		
+		else if(window.innerWidth < 1200)
+		{	styleLink.setAttribute("href", "mid.css");
+		}
+		
+		else if(window.innerWidth > 1200)
+		{	styleLink.setAttribute("href", "big.css");
+		}
+	}
+
+window.onresize = layoutHandler;
+layoutHandler();
+
+document.onmousedown=disableclick;
+status="Right click";
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     alert(status);
+     return false;    
+   }
+}
